01c47140edaa313b18b826b43d3edd835ee37729
Martin Jacob Zulu <jacob.zulu@cs.unza.zm>
2020-09-07 15:14:40 +0200

    Added user login functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.http import HttpResponse
+from django.contrib.auth import logout, login, authenticate
+from .forms import LoginForm
 
-# Create your views here.
+
+def login_view(request):
+    template = 'form.html'
+    form = LoginForm(request.POST or None)
+
+    if form.is_valid():
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return HttpResponse("login successful!")
+        
+    context = {
+        "form": form
+    }
+
+    return render(request, template, context)
